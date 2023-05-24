c99b5ff9c285735c6dc120e9c9b48b0c97e2e209
PRAVEEN KUMAR <117474739+AchintyaG@users.noreply.github.com>
2023-05-24 13:59:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-//your code here
-const guessno = document.getElementById("guess");
-var btn = document.getElementById("btn");
-var responce = document.getElementByTd("respond");
-function generateRandomNumber() {
-	var random = Math.floor(Math.random()*40)-20;
-	if(random>guessno)
-	{
-		responce.textContent="Getting Hotter";
-	}
-	else{
-		responce.textContent="guess higher";
-	}
-}
-btn.addEventListener("click", generateRandomNumber);
+var secretNumber = Math.floor(Math.random() * 100) + 1;
+    var previousGuess;
+    var previousDifference;
+
+    function checkGuess() {
+      var guess = parseInt(document.getElementById('guess').value);
+      var response = document.getElementById('respond');
+
+if (!previousGuess) {
+        previousGuess = guess;
+        previousDifference = Math.abs(guess - secretNumber);
+        response.innerText = 'Guess higher or lower.';
+      } else {
+        var currentDifference = Math.abs(guess - secretNumber);
+        if (currentDifference < previousDifference) {
+          response.innerText = 'Getting hotter.';
+          if (guess < secretNumber) {
+            response.innerText += ' Guess higher.';
+          } else if (guess > secretNumber) {
+            response.innerText += ' Guess lower.';
+          }
+        } else if (currentDifference > previousDifference) {
+          response.innerText = 'Getting colder.';
+          if (guess < secretNumber) {
+            response.innerText += ' Guess higher.';
+          } else if (guess > secretNumber) {
+            response.innerText += ' Guess lower.';
+          }
+        } else {
+          response.innerText = 'No change in temperature.';
+        }
+
+        previousGuess = guess;
+        previousDifference = currentDifference;
+      }
+    }
