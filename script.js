c95ac80b3a36c9307be61748cf6542b0c8745a31
PRAVEEN KUMAR <117474739+AchintyaG@users.noreply.github.com>
2023-05-24 13:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
-
+const guessno = document.getElementById("guess");
+var btn = document.getElementById("btn");
+var responce = document.getElementByTd("respond");
+function generateRandomNumber() {
+	var random = Math.floor(Math.random()*40)-20;
+	if(random>guessno)
+	{
+		responce.textContent="Getting Hotter";
+	}
+	else{
+		responce.textContent="guess higher";
+	}
+}
+btn.addEventListener("click", generateRandomNumber);
